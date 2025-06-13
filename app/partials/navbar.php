a3eb4e7b57cc83813b5365bd21c866318c50d059
INHOUSE-8 <inhouse8@ednindo.net>
2025-06-13 17:37:45 +0700

    redbui

--- app/partials/navbar.php
+++ app/partials/navbar.php
@@ -1,3 +1,14 @@
+<style>
+    body {
+        font-family: "Poppins", sans-serif;
+    }
+
+    .bg-body-tertiary {
+        background-color: #fff !important;
+        border-bottom: 1.45px solid #eeeeee;
+    }
+</style>
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
         <a class="navbar-brand fw-bold" href="https://ryunze.github.io/">Ryunze</a>
